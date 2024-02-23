48b5005684c4f9b2ca47505c2cd7299815f2bb12
789456123jay <156538618+789456123jay@users.noreply.github.com>
2024-02-22 21:58:47 -0600

    Updated hp_4

--- src/hp_4.py
+++ src/hp_4.py
@@ -8,27 +8,63 @@ from collections import defaultdict
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    pass
+    new_dates = []
+    for date_str in old_dates:
+        date_obj = datetime.strptime(date_str, '%y-%m-%d')
+        new_date_str = datetime.strftime(date_obj, '%d %b %y')
+        new_dates.append(new_date_str)
+    return new_dates
 
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
     a list of of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
-    pass
+    if not isinstance(start_date, str):
+        raise TypeError("Start date must be a string.")
+    elif not isinstance(n, int):
+        raise TypeError("Number of days must be an integer.")
+    else:
+        date_list = []
+            date_obj = datetime.strptime(start_date, '%Y-%m-%d') + timedelta(days=i)
+            date_list.append(date_obj)
+
+        return date_list
 
 
 def add_date_range(values, start_date):
     """Adds a daily date range to the list `values` beginning with
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
-    pass
+    start_date_object = datetime.strptime(start_date, '%Y-%m-%d')
+    date_seq = [start_date_object + timedelta(days=i) for i in range(len(values))]
+    res = list(zip(date_seq, values))
+    return res
 
 
 def fees_report(infile, outfile):
     """Calculates late fees per patron id and writes a summary report to
     outfile."""
-    pass
+    late_fees = defaultdict(float)
+
+    with open(infile, 'r') as csvfile:
+        reader = DictReader(csvfile)
+        for row in reader:
+            patron_id = row['patron_id']
+            date_returned = datetime.strptime(row['date_returned'], '%m/%d/%Y')
+            date_due = datetime.strptime(row['date_due'], '%m/%d/%Y')
+            late_days = max((date_returned - date_due).days, 0)
+            late_fee = late_days * 0.25
+            late_fees[patron_id] += late_fee
+    
+    with open(outfile, 'w') as csvfile:
+        writer = DictWriter(csvfile, fieldnames=['patron_id', 'late_fees'])
+        writer.writeheader()
+        for patron_id, fee in late_fees.items():
+            writer.writerow({
+            'patron_id': patron_id,
+            'late_fees': f'{fee:.2f}',
+            })
 
 
 # The following main selection block will only run when you choose
